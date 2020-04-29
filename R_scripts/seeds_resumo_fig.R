700f1a7636bbb957cea64473b9d4aa45cd8adee3
Talita Zupo <talita.zupo@gmail.com>
2020-04-29 17:11:19 -0300

    agora com a ultima figura (seeds survived heat temp)

--- R_scripts/seeds_resumo_fig.R
+++ R_scripts/seeds_resumo_fig.R
@@ -16,6 +16,7 @@ strat <- seeds[c(1:3),c(1,3)]
 buds <- seeds[c(4:6),c(1,3)]
 dorm <- seeds[c(7:8),c(1,3)]
 types <- seeds[c(9:11),c(1,3)]
+survived <- seeds[c(12:14), c(1,3)]
 
 #fazer graficos de barra
 plot(strat$estrategia, strat$prop_ssp)
@@ -127,7 +128,7 @@ dorm1 <- as.factor(dorm)
 strats2<-c("R+PT", "R+PS", "R+P-","R+PT", "R+PS", "R+P-")
 strat2 <-as.factor(strats2)
 
-val <- c("0.56", "0.43", "0", "0.96", "0.04", "0")
+val <- c("0.56", "0.43", "0", "0.96", "0", "0.04")
 val1 <- as.numeric(val)
 val1 <- val1*100
 
@@ -139,7 +140,7 @@ g1 <-ggplot(data=datax, aes(x=buds1, y=value1, fill = strat1,  width=.5)) + # wi
   geom_bar(position="stack", stat="identity")+
   scale_fill_manual(values=c('#CCCCCC','#666666', '#333333'))+
   scale_x_discrete(limits=c("Aunderground","Baerial", "Cbasal" ),
-                   labels=c("underground","aerial'", "basal" ))+
+                   labels=c("underground","aerial", "basal" ))+
   xlab("") +
   ylab("Proportion of species") +
   theme_classic() +
@@ -155,10 +156,10 @@ g2 <-ggplot(data=datay, aes(x=dorm1, y=val1, fill = strat2,  width=.5)) + # widt
   geom_bar(position="stack", stat="identity")+
   scale_fill_manual(values=c('#CCCCCC','#666666', '#333333'))+
   xlab("") +
-  ylab("Proportion of species") +
+  ylab("") +
   theme_classic() +
   theme (axis.text = element_text(size = 7), axis.title=element_text(size=8),
-         axis.text.x=element_text(size=8),
+         axis.text.x=element_text(size=8),axis.text.y = element_blank(),
          panel.grid.major=element_blank(),
          panel.grid.minor=element_blank(), panel.border=element_blank()) +
   theme(axis.line.x = element_line(color="black", size = 0), ## to write x and y axis again, ja que removi da borda
@@ -171,6 +172,29 @@ library(ggpubr)
 
 png("figs/figura04.png", res = 300, width = 2000, height = 800)
 ggarrange(g1, g2,
-          labels = c("a", "b"),label.x = c(0.14, 0.14),
+          labels = c("a", "b"),label.x = c(0.14, 0.09),
           label.y = 1, ncol = 2, nrow = 1)
+dev.off()
+
+#e se quiser colocar a legenda?
+#https://stackoverflow.com/questions/43220862/how-can-i-move-the-legend-position-with-grid-arrange
+
+# agora a ultima figura####
+
+g4<- ggplot(data=survived, aes(x= estrategia, y=prop_ssp,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
+  geom_bar(stat="identity", position=position_dodge(), colour="black")+
+
+  xlab("") +
+  ylab("Proportion of species") +
+  scale_y_continuous(limits = c(0, 100),breaks=0:20*25) +
+  theme_classic() +
+  theme (axis.text = element_text(size = 8), axis.title=element_text(size=8),
+         axis.text.x = element_text(size=8), axis.text.y = element_text(size=8), panel.grid.major=element_blank(),   # Pra tirar os grid lines
+         panel.grid.minor=element_blank(),panel.border=element_blank()) +
+  theme(axis.line.x = element_line(color = 'black', size = 0.5, linetype = 'solid'), ## to write x and y axis again, ja que removi da borda
+        axis.line.y = element_line(color='black', size = 0.5, linetype= 'solid'))+
+  theme(legend.position="none")
+
+png("figs/figura05.png", res = 300, width = 1200, height = 1000)
+grid.arrange(g4)
 dev.off()
