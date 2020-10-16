904a222f40c51a6fe0420a6eff4ac9ce6652cfbb
Talita Zupo <talita.zupo@gmail.com>
2020-10-16 10:28:49 -0300

    changing color and axis of figures

--- R_scripts/seeds_resumo_fig.R
+++ R_scripts/seeds_resumo_fig.R
@@ -25,10 +25,10 @@ survived <- seeds[c(12:14), c(1,3)]
 
 # as figuras
 f1<- ggplot(data=strat, aes(x= estrategia, y=prop_ssp,  width=.4)) + # width faz a barra ficar mais fina (ou grossa)
-  geom_bar(stat="identity", position=position_dodge(), colour="black")+
+  geom_bar(stat="identity", position=position_dodge(), fill ="#2171B5")+
 
   xlab("") +
-  ylab("Proportion of species") +
+  ylab("Species (%)") +
   scale_y_continuous(limits = c(0, 100),breaks=0:20*25) +
   theme_classic() +
   theme (axis.text = element_text(size = 8), axis.title=element_text(size=8),
@@ -40,11 +40,12 @@ f1<- ggplot(data=strat, aes(x= estrategia, y=prop_ssp,  width=.4)) + # width faz
 
 
 f2<- ggplot(data=buds, aes(x= estrategia, y=prop_ssp,  width=.4)) + # width faz a barra ficar mais fina (ou grossa)
-  geom_bar(stat="identity", position=position_dodge(), colour="black")+
+  geom_bar(stat="identity", position=position_dodge(), fill ="#2171B5")+
 
   xlab("") +
   ylab("") +
   scale_y_continuous(limits = c(0, 100),breaks=0:20*25) +
+
   theme_classic() +
   theme (axis.text = element_text(size = 8), axis.title=element_text(size=8),
          axis.text.x = element_text(size=8),axis.text.y = element_blank(), panel.grid.major=element_blank(),   # Pra tirar os grid lines
@@ -54,10 +55,10 @@ f2<- ggplot(data=buds, aes(x= estrategia, y=prop_ssp,  width=.4)) + # width faz 
   theme(legend.position="none")
 
 f3<- ggplot(data=dorm, aes(x= estrategia, y=prop_ssp,  width=.4)) + # width faz a barra ficar mais fina (ou grossa)
-  geom_bar(stat="identity", position=position_dodge(), colour="black")+
+  geom_bar(stat="identity", position=position_dodge(), fill ="#2171B5")+
 
   xlab("") +
-  ylab("Proportion of species") +
+  ylab("Species (%)") +
   scale_y_continuous(limits = c(0, 100),breaks=0:20*25) +
   theme_classic() +
   theme (axis.text = element_text(size = 8), axis.title=element_text(size=8),
@@ -68,7 +69,7 @@ f3<- ggplot(data=dorm, aes(x= estrategia, y=prop_ssp,  width=.4)) + # width faz 
   theme(legend.position="none")
 
 f4<- ggplot(data=types, aes(x= estrategia, y=prop_ssp,  width=.4)) + # width faz a barra ficar mais fina (ou grossa)
-  geom_bar(stat="identity", position=position_dodge(), colour="black")+
+  geom_bar(stat="identity", position=position_dodge(),fill ="#2171B5")+
 
   xlab("") +
   ylab("") +
@@ -84,28 +85,35 @@ f4<- ggplot(data=types, aes(x= estrategia, y=prop_ssp,  width=.4)) + # width faz
 
 #salvando todos os graphs em uma figura:
 
-png("figs/figura02b.png", res = 300, width = 2400, height = 1200)
-grid.arrange(f1, f2, f3, f4, ncol=2)
-dev.off()
+#png("figs/figura02b.png", res = 300, width = 2400, height = 1200)
+#grid.arrange(f1, f2, f3, f4, ncol=2)
+#dev.off()
 
 #para adicionar labels dentro do plot usando vjust and hjust:
 
-png("figs/figura01.png", res = 300, width = 2400, height = 1200)
-ggarrange(f1, f2, f3, f4,
+#png("figs/figura01.png", res = 300, width = 2400, height = 1200)
+#ggarrange(f1, f2, f3, f4,
           labels = c("a", "b", "c", "d"),vjust =  1.0, hjust = -0.5,
           ncol = 2, nrow = 2)
-dev.off()
+#dev.off()
 
+##============ THIS IS FIGURE 1 OF ZUPO ET AL ============##
 #agora usando label.x e label.y:
 # que é melhor que vjust and hjust - dá pra ajustar cada label onde vc quer!
 
-png("figs/figura03.png", res = 300, width = 2000, height = 1200)
+#BLACK AND WHITE
+png("figs/figura04.png", res = 300, width = 2000, height = 1200)
 ggarrange(f1, f2, f3, f4,
           labels = c("a", "b", "c", "d"),label.x = c(0.14, 0.09, 0.14, 0.09),
           label.y = 1, ncol = 2, nrow = 2)
 dev.off()
 
-
+#COLORS
+png("figs/figura04_COR.png", res = 300, width = 2000, height = 1200)
+ggarrange(f1, f2, f3, f4,
+          labels = c("a", "b", "c", "d"),label.x = c(0.14, 0.09, 0.14, 0.09),
+          label.y = 1, ncol = 2, nrow = 2)
+dev.off()
 ##################  agora pra fazer a figura 2  ####################
 
 #essa figura é a antiga fig 3 e 5 juntas. stacked barplots
@@ -118,7 +126,7 @@ buds1 <- as.factor(buds)
 strats<-c("R+PT", "R+PS", "R+P-","R+PT", "R+PS", "R+P-","R+PT", "R+PS", "R+P-" )
 strat1 <-as.factor(strats)
 
-values <- c( "0.89", "0.11", "0", "0.75","0.25", "0", '0.8', "0.16", "0.04")
+values <- c( "0.89", "0.11", "0", "0.8","0.2", "0", '0.77', "0.19", "0.04")
 value1 <-as.numeric(values)
 value1 <- value1*100
 datax <- data.frame(buds1, strat1, value1)
@@ -136,38 +144,41 @@ val1 <- val1*100
 
 datay <- data.frame(dorm1, strat2, val1)
 
-#terceiro (growth forms, com sub-shrubs e shrubs separados)
-gf <- c( "Forb", "Forb", "Forb","Graminoid", "Graminoid", "Graminoid", "Sub-shrub", "Sub-shrub","Sub-shrub", "Shrub", "Shrub", "Shrub")
+#terceiro
+gf <- c( "Forb", "Forb", "Forb","Graminoid", "Graminoid", "Graminoid", "Shrub", "Shrub", "Shrub")
 gf1 <- as.factor(gf)
 
-strats3a<-c("R+PT", "R+PS", "R+P-","R+PT", "R+PS", "R+P-", "R+PT", "R+PS", "R+P-","R+PT", "R+PS", "R+P-")
+strats3a<-c("R+PT", "R+PS", "R+P-","R+PT", "R+PS", "R+P-", "R+PT", "R+PS", "R+P-")
 strat3 <-as.factor(strats3a)
 
-values <-c( "1", "0", "0","0.83", "0.17","0", "1", "0", "0", "0.7", "0.26", "0.04")
+values <-c( "1", "0", "0","0.8", "0.2","0", "0.73", "0.23", "0.04")
 val3 <- as.numeric(values)
 val3 <- val3*100
 
 dataz <-data.frame(gf1, strat3, val3)
 
-#terceiro (growth forms, com sub-shrubs e shrubs juntos)
-gf2<-c( "Forb", "Forb", "Forb","Graminoid", "Graminoid", "Graminoid", "Shrub", "Shrub", "Shrub")
-gf2 <- as.factor(gf2)
 
-straty <-c("R+PT", "R+PS", "R+P-", "R+PT", "R+PS", "R+P-","R+PT", "R+PS", "R+P-")
-straty <-as.factor(straty)
-
-valuex <- c("1", "0", "0", "0.83", "0.17","0", "0.75", "0.21", "0.04")
-val4 <- as.numeric(valuex)
-val4 <- val4*100
-
-datazz <-data.frame(gf2, straty, val4)
 
 #as figuras:
+library(RColorBrewer)
+display.brewer.pal(n = 9, name = 'Dark2') # para ver as cores
+brewer.pal(n=9, name = "Dark2")
+c("#E7298A", "#E6AB02", "#2171B5")
+
+display.brewer.pal(n = 9, name = 'Blues') # para ver as cores
+brewer.pal(n=9, name = "Blues")
+
+library("ggplot2")
+library("ggsci")
+
+library(wesanderson)
+wes_palette("GrandBudapest1", n = 3)
+
 g1 <-ggplot(data=datax, aes(x=buds1, y=value1, fill = strat1,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
   geom_bar(position="stack", stat="identity")+
-  scale_fill_manual(values=c('#CCCCCC','#666666', '#333333'))+
+  scale_fill_manual(values = wes_palette("Darjeeling1", n = 3))+
     xlab("") +
-  ylab("Proportion of species") +
+  ylab("Species (%)") +
   theme_classic() +
   theme (axis.text = element_text(size = 7), axis.title=element_text(size=8),
          axis.text.x=element_text(size=8),
@@ -177,6 +188,21 @@ g1 <-ggplot(data=datax, aes(x=buds1, y=value1, fill = strat1,  width=.5)) + # wi
         axis.line.y = element_line(color="black", size = 0))+
   theme(legend.position="bottom")
 g1<- g1+labs(fill ="")
+
+g1_cor <-ggplot(data=datax, aes(x=buds1, y=value1, fill = strat1,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
+  geom_bar(position="stack", stat="identity")+
+  scale_fill_manual(values=c("#E7298A", "#E6AB02", "#2171B5"))+
+  xlab("") +
+  ylab("Species (%)") +
+  theme_classic() +
+  theme (axis.text = element_text(size = 7), axis.title=element_text(size=8),
+         axis.text.x=element_text(size=8),
+         panel.grid.major=element_blank(),
+         panel.grid.minor=element_blank(), panel.border=element_blank()) +
+  theme(axis.line.x = element_line(color="black", size = 0), ## to write x and y axis again, ja que removi da borda
+        axis.line.y = element_line(color="black", size = 0))+
+  theme(legend.position="bottom")
+g1c<- g1_cor+labs(fill ="")
 
 g2 <-ggplot(data=datay, aes(x=dorm1, y=val1, fill = strat2,  width=.35)) + # width faz a barra ficar mais fina (ou grossa)
   geom_bar(position="stack", stat="identity")+
@@ -193,27 +219,43 @@ g2 <-ggplot(data=datay, aes(x=dorm1, y=val1, fill = strat2,  width=.35)) + # wid
   theme(legend.position="bottom")
 g2<-g2+ labs(fill ="")
 
-g2b <-ggplot(data=datay, aes(x=dorm1, y=val1, fill = strat2,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
+g2_cor <-ggplot(data=datay,  aes(x=dorm1, y=val1, fill = strat2,  width=.35)) + # width faz a barra ficar mais fina (ou grossa)
   geom_bar(position="stack", stat="identity")+
-  scale_fill_manual(values=c('#CCCCCC','#666666', '#333333'))+
+  scale_fill_manual(values=c("#E7298A", "#E6AB02", "#2171B5"))+
   xlab("") +
   ylab("") +
   theme_classic() +
   theme (axis.text = element_text(size = 7), axis.title=element_text(size=8),
-         axis.text.x=element_text(size=8),axis.text.y = element_text(size=8),
+         axis.text.x=element_text(size=8),axis.text.y = element_blank(),
          panel.grid.major=element_blank(),
          panel.grid.minor=element_blank(), panel.border=element_blank()) +
   theme(axis.line.x = element_line(color="black", size = 0), ## to write x and y axis again, ja que removi da borda
         axis.line.y = element_line(color="black", size = 0))+
   theme(legend.position="bottom")
-g2b<-g2b + labs(fill ="")
+g2c<- g2_cor+labs(fill ="")
 
-#sub-shrubs separado
-g3 <-ggplot(data=dataz, aes(x=gf1, y=val3, fill = strat3,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
+#no
+#g2b <-ggplot(data=datay, aes(x=dorm1, y=val1, fill = strat2,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
+ # geom_bar(position="stack", stat="identity")+
+ # scale_fill_manual(values=c('#CCCCCC','#666666', '#333333'))+
+#  xlab("") +
+ # ylab("") +
+ # theme_classic() +
+ # theme (axis.text = element_text(size = 7), axis.title=element_text(size=8),
+         axis.text.x=element_text(size=8),axis.text.y = element_text(size=8),
+         panel.grid.major=element_blank(),
+         panel.grid.minor=element_blank(), panel.border=element_blank()) +
+  #theme(axis.line.x = element_line(color="black", size = 0), ## to write x and y axis again, ja que removi da borda
+     #   axis.line.y = element_line(color="black", size = 0))+
+  #theme(legend.position="bottom")
+#g2b<-g2b + labs(fill ="")
+
+#
+g3_cor <-ggplot(data=dataz, aes(x=gf1, y=val3, fill = strat3,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
   geom_bar(position="stack", stat="identity")+
-  scale_fill_manual(values=c('#CCCCCC','#666666', '#333333'))+
+  scale_fill_manual(values=c("#E7298A", "#E6AB02", "#2171B5"))+
   xlab("") +
-  ylab("Proportion of species") +
+  ylab("Species (%)") +
   theme_classic() +
   theme (axis.text = element_text(size = 7), axis.title=element_text(size=8),
          axis.text.x=element_text(size=8),axis.text.y = element_text(size=8),
@@ -222,23 +264,23 @@ g3 <-ggplot(data=dataz, aes(x=gf1, y=val3, fill = strat3,  width=.5)) + # width 
   theme(axis.line.x = element_line(color="black", size = 0), ## to write x and y axis again, ja que removi da borda
         axis.line.y = element_line(color="black", size = 0))+
   theme(legend.position="bottom")
-g3<-g3+ labs(fill ="")
+g3c<-g3_cor+ labs(fill ="")
 
 #sub-shrubs junto
-g4 <-ggplot(data=datazz, aes(x=gf2, y=val4, fill = straty,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
-  geom_bar(position="stack", stat="identity")+
-  scale_fill_manual(values=c('#CCCCCC','#666666', '#333333'))+
-  xlab("") +
-  ylab("Proportion of species") +
-  theme_classic() +
-  theme (axis.text = element_text(size = 7), axis.title=element_text(size=8),
-         axis.text.x=element_text(size=8),axis.text.y = element_text(size=8),
-         panel.grid.major=element_blank(),
-         panel.grid.minor=element_blank(), panel.border=element_blank()) +
-  theme(axis.line.x = element_line(color="black", size = 0), ## to write x and y axis again, ja que removi da borda
-        axis.line.y = element_line(color="black", size = 0))+
-  theme(legend.position="bottom")
-g4<-g4+ labs(fill ="")
+#g4 <-ggplot(data=datazz, aes(x=gf2, y=val4, fill = straty,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
+  #geom_bar(position="stack", stat="identity")+
+ # scale_fill_manual(values=c('#CCCCCC','#666666', '#333333'))+
+  #xlab("") +
+ # ylab("Proportion of species") +
+ # theme_classic() +
+  #theme (axis.text = element_text(size = 7), axis.title=element_text(size=8),
+      #   axis.text.x=element_text(size=8),axis.text.y = element_text(size=8),
+       #  panel.grid.major=element_blank(),
+       #  panel.grid.minor=element_blank(), panel.border=element_blank()) +
+ # theme(axis.line.x = element_line(color="black", size = 0), ## to write x and y axis again, ja que removi da borda
+     #   axis.line.y = element_line(color="black", size = 0))+
+ # theme(legend.position="bottom")
+#g4<-g4+ labs(fill ="")
 
 
 #e se quiser colocar a legenda?
@@ -262,96 +304,44 @@ g4<-g4+ labs(fill ="")
 library(ggpubr)
 
 #primeira fig sem a legenda
-png("figs/figura04.png", res = 300, width = 2000, height = 800)
+png("figs/figura05.png", res = 300, width = 2000, height = 800)
 ggarrange(g1, g2,
           labels = c("a", "b"),label.x = c(0.14, 0.09), ncol=2, nrow=1)
 dev.off()
 
 #segunda fig com a legenda!
-png("figs/figura04B.png", res = 300, width = 2000, height = 800)
+png("figs/figura05a.png", res = 300, width = 2000, height = 800)
 ggarrange(g1, g2, labels = c("a", "b"),label.x = c(0.14, 0.09),
 common.legend = TRUE, legend = "bottom")
 dev.off()
 
 #adding growth forms na figura
 #com subshrubs separado
-png("figs/figura04ca.png", res = 300, width = 2000, height = 1400)
-ggarrange(g1, g3, g2, labels = c("a", "b", "c"),label.x = c(0.14, 0.09, 0.14),
+#png("figs/figura05b.png", res = 300, width = 2000, height = 1400)
+#ggarrange(g1, g3, g2, labels = c("a", "b", "c"),label.x = c(0.14, 0.09, 0.14),
+          common.legend = TRUE, legend = "bottom")
+#dev.off()
+
+####============   ESSA AQUI é a figura 2 do Zupo et al. ===============####
+#===========================================================================
+
+#BLACK and GREY
+png("figs/figura05d.png", res = 300, width = 2000, height = 1400)
+ggarrange(g1, g2, g3, labels = c("a", "b", "c"),label.x = c(0.14, 0.09, 0.14),
           common.legend = TRUE, legend = "bottom")
 dev.off()
 
-#shrubs e sub-shrubs juntos
-png("figs/figura04d.png", res = 300, width = 2000, height = 1400)
-ggarrange(g1, g2, g4, labels = c("a", "b", "c"),label.x = c(0.14, 0.09, 0.14),
+## == COLORIDA ==
+png("figs/figura05_cor2.png", res = 300, width = 2000, height = 1400)
+ggarrange(g1c, g2c, g3c, labels = c("a", "b", "c"),label.x = c(0.14, 0.09, 0.14),
           common.legend = TRUE, legend = "bottom")
 dev.off()
+
+#======================================================================
 
 #outro jeito de arrange:
-png("figs/figura05.png", res = 300, width = 1100, height = 2000)
-ggarrange(g1, g4, g2b, ncol = 1, labels = c("a", "b", "c"),label.x = c(0.14, 0.14, 0.14),common.legend = TRUE, legend = "bottom")
-dev.off()
+#png("figs/figura07.png", res = 300, width = 1100, height = 2000)
+#ggarrange(g1, g4, g2b, ncol = 1, labels = c("a", "b", "c"),label.x = c(0.14, 0.14, 0.14),common.legend = TRUE, legend = "bottom")
+#dev.off()
 
 
-##################  agora a ultima figura    ##################
-####### OBS: a ultima figura ta em outro script -> last_fig #
-
-survived
-g4<- ggplot(data=survived, aes(x= estrategia, y=prop_ssp,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
-  geom_bar(stat="identity", position=position_dodge(), colour="black")+
-
-  xlab("") +
-  ylab("Proportion of species") +
-  scale_y_continuous(limits = c(0, 100),breaks=0:20*25) +
-  theme_classic() +
-  theme (axis.text = element_text(size = 8), axis.title=element_text(size=8),
-         axis.text.x = element_text(size=8), axis.text.y = element_text(size=8), panel.grid.major=element_blank(),   # Pra tirar os grid lines
-         panel.grid.minor=element_blank(),panel.border=element_blank()) +
-  theme(axis.line.x = element_line(color = 'black', size = 0.5, linetype = 'solid'), ## to write x and y axis again, ja que removi da borda
-        axis.line.y = element_line(color='black', size = 0.5, linetype= 'solid'))+
-  theme(legend.position="none")
-
-png("figs/figura05.png", res = 300, width = 1200, height = 1000)
-grid.arrange(g4)
-dev.off()
-
-
-###################agora a ultima figura como stacked barplot###################
-######### OBS: essa nao é a figura q foi pro paper. olhar script last_fig
-
-#primeiro o dataframe
-
-#exp<-c("100-1 min", "100-1 min", "100-1 min", "100-3 min", "100-3 min", "100-3 min", "200-1 min", "200-1 min","200-1 min")
-#exp1 <- as.factor(exp)
-
-#strats<-c("Stimulated", "Tolerated", "Decreased","Stimulated", "Tolerated", "Decreased","Stimulated", "Tolerated", "Decreased" )
-#strat2 <-as.factor(strats)
-
-#values <- c('0.02', "0.95", "0.02", "0.14", "0.77", "0.09", "0.12","0.34", "0.54")
-#value2 <-as.numeric(values)
-#valuex <- value2*100
-#data2 <- data.frame(exp1, strat2, valuex)
-
-
-#a figura:
-
-g5 <-ggplot(data=data2, aes(x=exp1, y=valuex, fill = strat2,  width=.5)) + # width faz a barra ficar mais fina (ou grossa)
-  geom_bar(position="stack", stat="identity")+
-  scale_fill_manual(values=c('#CCCCCC','#666666', '#333333'))+
-  scale_x_discrete(limits=c("100-1 min","100-3 min", "200-1 min" ),
-                   labels=c("100 - 1 min","100 - 3 min", "200 - 1 min" ))+
-    xlab("") +
-  ylab("Proportion of species") +
-  theme_classic() +
-  theme (axis.text = element_text(size = 7), axis.title=element_text(size=8),
-         axis.text.x=element_text(size=8),
-         panel.grid.major=element_blank(),
-         panel.grid.minor=element_blank(), panel.border=element_blank()) +
-  theme(axis.line.x = element_line(color="black", size = 0), ## to write x and y axis again, ja que removi da borda
-        axis.line.y = element_line(color="black", size = 0))+
-  theme(legend.position="bottom", legend.text = element_text(size =9), legend.key.size = unit(0.35, "cm"))
-g5<- g5+labs(fill ="")
-
-png("figs/figura05B.png", res = 300, width = 1200, height = 1000)
-ggarrange(g5,
-          common.legend = TRUE, legend = "bottom")
-dev.off()
